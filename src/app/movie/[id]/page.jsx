419dbb1dfee6cede9845ba1f900e748c33c825c3
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-04-27 21:46:19 +0300

    Add Movie page

---
+++ src/app/movie/[id]/page.jsx
@@ -0,0 +1,62 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const API_KEY = process.env.API_KEY;
+
+export default async function MoviePage({ params }) {
+  const movieId = params.id;
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}`
+  );
+  const movie = await res.json();
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  console.log(movie);
+  return (
+    <div className="w-full">
+      <div className="flex flex-col p-4 lg:pt-8 lg:flex-row content-center max-w-6xl mx-auto lg:space-x-6">
+        <Image
+          src={`https://image.tmdb.org/t/p/original${
+            movie.backdrop_path || movie.poster_path
+          }`}
+          alt={`${movie.title}`}
+          width={500}
+          height={300}
+          className="rounded-lg group-hover:opacity-75 transition-opacity duration-300"
+          style={{ maxWidth: "100%", height: "100%" }}
+        ></Image>
+        <div className="p-2">
+          <h2 className="text-2xl font-semibold mb-3">
+            {movie.title || movie.original_name}
+          </h2>
+          <p className="text-lg mb-3">
+            <span className="font-semibold mr-1">Overview: </span>
+            {movie.overview}
+          </p>
+          <p className="mb-3">
+            <span className="font-semibold mr-1">Date released: </span>
+            {movie.release_date || movie.first_air_date}
+          </p>
+          <p className="mb-3">
+            <span className="font-semibold mr-1">Duration: </span>
+            {movie.runtime} minutes
+          </p>
+          <p className="mb-3">
+            <span className="font-semibold mr-1">Rating: </span>
+            {movie.vote_count}
+          </p>
+          {movie.genres && (
+            <p className="mb-3">
+              <span className="font-semibold mr-1">Genre: </span>
+              {movie.genres.map((g) => `${g.name}`).join(", ")}
+            </p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,35 +5,34 @@ import { FiThumbsUp } from "react-icons/fi";
 
 export default function Card({ result }) {
   return (
-    <div
-      className="p-4 group cursor-pointer sm:hover:shadow-neutral-700
+    <Link href={`/movie/${result.id}`}>
+      <div
+        className="p-4 group cursor-pointer sm:hover:shadow-neutral-700
     sm:shadow-md rounded-lg sm:border border-neutral-500 sm:m-2 transition-shadow duration-200"
-    >
-      <Link href={`/movie/${result.id}`}>
-        <div>
-          <Image
-            src={`https://image.tmdb.org/t/p/original${
-              result.backdrop_path || result.poster_path
-            }`}
-            alt={`${result.title}`}
-            width={500}
-            height={300}
-            className="sm:rounded-t-lg w-full group-hover:opacity-75 transition-opacity duration-300"
-          ></Image>
-        </div>
-      </Link>
-      <div className="p-2">
-        <h2 className="text-lg font-semibold truncate">
-          {result.title || result.original_name}
-        </h2>
-        <p className="flex items-center text-sm">
-          {result.release_date || result.first_air_date}
-          <FiThumbsUp className="h-5 mr-1 ml-3" />
-          {result.vote_count}
-        </p>
+      >
+        <Image
+          src={`https://image.tmdb.org/t/p/original${
+            result.backdrop_path || result.poster_path
+          }`}
+          alt={`${result.title}`}
+          width={500}
+          height={300}
+          className="sm:rounded-t-lg w-full group-hover:opacity-75 transition-opacity duration-300"
+        ></Image>
 
-        <p className="line-clamp-2 text-md mt-2">{result.overview}</p>
+        <div className="p-2">
+          <h2 className="text-lg font-semibold truncate">
+            {result.title || result.original_name}
+          </h2>
+          <p className="flex items-center text-sm">
+            {result.release_date || result.first_air_date}
+            <FiThumbsUp className="h-5 mr-1 ml-3" />
+            {result.vote_count}
+          </p>
+
+          <p className="line-clamp-2 text-md mt-2">{result.overview}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
