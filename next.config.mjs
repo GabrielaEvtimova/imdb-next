a524ea470e437b6a57306aed64123e1af9043617
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-04-28 00:53:10 +0300

    Disable rule in next.config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'image.tmdb.org',
-            pathname: '**',
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        pathname: "**",
       },
-    // images: {
-    //     domains: ['image.tmdb.org']
-    // }
+    ],
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+  // images: {
+  //     domains: ['image.tmdb.org']
+  // }
 };
+
+// module.exports = nextConfig
 
 export default nextConfig;
