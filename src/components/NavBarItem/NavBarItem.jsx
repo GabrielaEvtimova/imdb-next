99026cb4035bcc0caef15a5c67f1bad3d427813a
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-04-28 00:25:47 +0300

    Add Suspense from react in NavBarItem

--- src/components/NavBarItem/NavBarItem.jsx
+++ src/components/NavBarItem/NavBarItem.jsx
@@ -3,22 +3,25 @@
 import Link from "next/link";
 import React from "react";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function NavBarItem({ title, param }) {
   const searchParams = useSearchParams();
   const genre = searchParams.get("genre");
   return (
-    <div>
-      <Link
-        className={`hover:text-amber-100 dark:text-amber-100 hover:shadow-2xl dark:hover:text-neutral-900 font-semibold
+    <Suspense>
+      <div>
+        <Link
+          className={`hover:text-amber-100 dark:text-amber-100 hover:shadow-2xl dark:hover:text-neutral-900 font-semibold
         ${
           genre === param &&
           "underline underline-offset-8 decoration-2 decoration-neutral-900 hover:decoration-amber-100 dark:decoration-amber-100 dark:hover:decoration-neutral-800 rounded-xl"
         }`}
-        href={`/?genre=${param}`}
-      >
-        {title}
-      </Link>
-    </div>
+          href={`/?genre=${param}`}
+        >
+          {title}
+        </Link>
+      </div>
+    </Suspense>
   );
 }
